6906ac20ae872e6910508a3873d570e33840db33
Darcisio Almeida <darcisioalmeida@gmail.com>
2022-10-13 18:29:23 -0300

    Definindo configurações final do Backend
    Criando aula, definindo framework - djangorestframework, criando serializers

--- backend/myteacher/urls.py
+++ backend/myteacher/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from teacher.views import ProfessorList, CadastrarAulaAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('professores/', ProfessorList.as_view()),
+    path('professores/<int:id>/aulas', CadastrarAulaAPIView.as_view())
 ]
